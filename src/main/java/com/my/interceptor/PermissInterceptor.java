11b61859edfd4f4bdb0d49f17e353fa16ee2b0d0
Admin <https://gitee.com/tx_8_0/tx.git>
2023-06-18 23:37:08 +0800

    UniversityOffice

--- src/main/java/com/my/interceptor/PermissInterceptor.java
+++ src/main/java/com/my/interceptor/PermissInterceptor.java
@@ -30,8 +30,6 @@ public class PermissInterceptor implements HandlerInterceptor {
      * @param handler
      * @return true为放行 false拦截
      * @throws Exception
-     *
-     *
      */
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
